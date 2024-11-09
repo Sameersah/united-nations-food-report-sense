e25efd101801e098bfe988415e1cb6b2112ee386
Sarvesh Borkar <sarvesh.borkar@sjsu.edu>
2024-11-09 12:29:40 -0800

    added orchestrator-service

--- orchestrator-service/orchestrator.py
+++ orchestrator-service/orchestrator.py
@@ -2,7 +2,7 @@ import os
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 os.environ["OPENAI_API_KEY"] = "<API_KEY>"
-documents = SimpleDirectoryReader("./data").load_data()
+documents = SimpleDirectoryReader("../data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
 
